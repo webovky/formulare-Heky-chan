fe44a130ec56b42a8c2a0150c384606d7eb08661
Heky-chan <71437946+Heky-chan@users.noreply.github.com>
2020-11-18 14:40:14 +0100

    Add files via upload
    přidání funkce flash a omezení přístupu

--- formulare/webface/routes.py
+++ formulare/webface/routes.py
@@ -1,9 +1,10 @@
 from . import app
-from flask import render_template, request, redirect, session, url_for, abort
+from flask import render_template, request, redirect, session, url_for, abort, flash
 """
 import os
 os.urandom(24)
 """
+
 app.secret_key = '123456789'
 
 @app.route('/')
@@ -22,44 +23,52 @@ def info_post():
     heslo = request.form.get('heslo')
     if heslo == 'heslo':
         session['user'] = jmeno
+        flash("Jste přihlášen")
     print(jmeno,heslo)
     return redirect(url_for('index'))
 
 @app.route('/logout/',methods=['GET','POST'])
 def logout():
     session.pop('user', None)
-    return redirect(url_for('index'))
+    flash("Jste odhlášen")
+    return redirect(url_for('info'))
 
 @app.route('/tajné/',methods=['GET'])
 def tajné():
     if 'user' in session:
         return render_template('tajné.html.j2')
     else:
-        return abort(403)
+        return redirect(url_for('info'))
 
 @app.route('/obdelník/')
 def obdelník():
     title = 'Obdelník'
-    x = request.args.get('x')
-    y = request.args.get('y')
-    try:
-        z = int(x) * int(y)
-        n = 2 * (int(x) + int(y))
-    except (TypeError, ValueError):
-        z = ''
-        n = ''
-    return render_template('obdelník.html.j2', title=title, z=z, n=n)
+    if 'user' in session:
+        x = request.args.get('x')
+        y = request.args.get('y')
+        try:
+            z = int(x) * int(y)
+            n = 2 * (int(x) + int(y))
+        except (TypeError, ValueError):
+            z = ''
+            n = ''
+        return render_template('obdelník.html.j2', title=title, z=z, n=n)
+    else:
+        return redirect(url_for('info'))
 
 @app.route('/čtverec/')
 def čtverec():
     title = 'Čtverec'
-    x = request.args.get('x')
-    try:
-        z = int(x) * int(x)
-        n = 4 * int(x)
-    except (TypeError, ValueError):
-        z = ''
-        n = ''
-    return render_template('čtverec.html.j2', title=title, z=z, n=n)
+    if 'user' in session:
+        x = request.args.get('x')
+        try:
+            z = int(x) * int(x)
+            n = 4 * int(x)
+        except (TypeError, ValueError):
+            z = ''
+            n = ''
+        return render_template('čtverec.html.j2', title=title, z=z, n=n)
+    else:
+        return redirect(url_for('info'))
 
 
